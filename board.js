6510900f616f0a7389799d1e4a6d5326c9de7fd5
Nick Battjes <nhbattjes@gmail.com>
2018-10-04 15:12:40 -0700

    getting started

--- board.js
+++ board.js
@@ -1,9 +1,23 @@
+class Space {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+        
+    }
+}
+
 class Board {
     constructor() {
-        this.board = this.boardGridGenerator();
+        this.board = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
     }
 
     boardGridGenerator() {
-        
+        let board = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    }
+
+    updateBoard(board, mark, pos) {
+        let row = pos[0];
+        let column = pos[1];
+        board[row][column] = mark;
     }
 }
--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+
+
 class Game {
     constructor(ctx) {
     //Canvas
@@ -5,6 +7,21 @@ class Game {
     this.canvasHeight = 500;
     this.ctx = ctx;
     this.board = new Board();
+    }
+
+    drawBoard(board) {
+        this.ctx.beginPath();
+        this.ctx.rect(this.canvasWidth / 3 - 10, 0, this.canvasWidth / 3, this.canvasHeight);
+        this.ctx.rect(this.canvasWidth * 0.66 - 10, 0, this.canvasWidth * 0.66, this.canvasHeight);
+        this.ctx.rect(0, this.canvasHeight / 3 - 10, this.canvasWidth, this.canvasHeight / 3);
+        this.ctx.rect(0, this.canvasHeight * 0.66 - 10, this.canvasWidth, this.canvasHeight * 0.66);
+        // for(let i = 0; i < this.board.length; i++) {
+        //     for (let j = 0; j < this.board[i].length; j++) {
+        //         if (this.board[i][j] === 'X') {
+        //         this.drawX()
+        //         }
+        //     }
+        // }
     }
 
 
