014cfa4e8c4e7ccb003b24d6b2a530fef922c355
Nick Battjes <nhbattjes@gmail.com>
2018-10-08 19:51:01 -0700

    O mostly works

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,31 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
     game.drawBoard();
 });
 
-
-
 class Game {
     constructor(ctx) {
         //Canvas
         this.canvasWidth = 700;
-        this.canvasHeight = 500;
+        this.canvasHeight = 700;
         this.ctx = ctx;
         this.mark1 = "X";
         this.mark2 = "Y";
         this.firstColumnLine = this.canvasWidth / 3;
-        this.secondColumnLine = this.canvasWidth * 0.66;
+        this.secondColumnLine = this.canvasWidth * (2/3);
         this.firstRowLine = this.canvasHeight / 3;
-        this.secondRowLine = this.canvasHeight * 0.66;
+        this.secondRowLine = this.canvasHeight * (2/3);
         this.readClick = this.readClick.bind(this);
     }
 
     drawBoard() {
         this.ctx.fillStyle = "#ffffff";
         //first column
-        this.ctx.rect(this.firstColumnLine - 10, 0, 10, this.canvasHeight);
+        this.ctx.rect(this.firstColumnLine - 5, 0, 10, this.canvasHeight);
         //second column
-        this.ctx.rect(this.secondColumnLine - 10, 0, 10, this.canvasHeight);
-        this.ctx.rect(0, this.firstRowLine - 10, this.canvasWidth, 10);
-        this.ctx.rect(0, this.secondRowLine - 10, this.canvasWidth, 10);
+        this.ctx.rect(this.secondColumnLine - 5, 0, 10, this.canvasHeight);
+        this.ctx.rect(0, this.firstRowLine - 5, this.canvasWidth, 10);
+        this.ctx.rect(0, this.secondRowLine - 5, this.canvasWidth, 10);
         this.ctx.fill();
    
     }
@@ -47,11 +45,14 @@ class Game {
         if (x < this.firstColumnLine) {
             if (y < this.firstRowLine) {
                 console.log("top left");
+                this.drawO((this.firstColumnLine - 10) / 2, (this.firstRowLine - 10) / 2);
             }
             if (y > this.firstRowLine && y < this.secondRowLine) {
+                this.drawO((this.firstColumnLine - 10) / 2, (this.canvasHeight / 2) - 7.5);
                 console.log("middle left");
             }
             if (y > this.secondRowLine) {
+                this.drawO((this.firstColumnLine - 10) / 2, this.canvasHeight * (5/6));
                 console.log("bottom left");
             }
         }
@@ -79,6 +80,21 @@ class Game {
         }
     }
 
+    drawX(x, y) {
+        this.beginPath();
+        
+    }
+
+    drawO(x, y){
+        console.log("in drawO");
+        console.log(x);
+        console.log(y);
+        this.ctx.beginPath();
+        this.ctx.arc(x, y, this.firstColumnLine / 2 - 10, 0, Math.PI * 2);
+        this.ctx.fillStyle = "#ffffff";
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
 
     bindKeys() {
         document.getElementById("gameCanvas").addEventListener("click", this.readClick.bind(this), false);
